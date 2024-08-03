245cc06bb3ed2c21f6175b8b7a9d2c738f7fcf9d
yaylinda <lindazheng1993@gmail.com>
2024-08-02 20:53:27 -0500

    add balls

---
+++ balls.py
@@ -0,0 +1,17 @@
+def main():
+    balls = [
+        ["brown", "green", "brown", "light green"],
+        ["light green", "gray", "light blue", "green"],
+        ["red", "green", "light green", "yellow"],
+        ["pink", "red", "yellow", "purple"],
+        ["light green", "pink", "red", "green"],
+        ["gray", "blue", "pink", "gray"],
+        ["orange", "blue", "yellow", "dark green"],
+        ["purple", "orange", "orange", "dark green"],
+        ["light blue", "blue", "orange", "red"],
+        ["purple", "dark green", "dark green", "light blue"],
+        ["yellow", "pink", "purple", "blue"],
+        ["light blue", "brown", "brown", "gray"],
+        ["", "", "", ""],
+        ["", "", "", ""],
+    ]
